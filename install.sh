343b69f7cbe6f7c8552aa5e4c9ebf11729a1cc6e
palonza <palonza@gmail.com>
2024-02-12 01:45:21 -0300

    add change network -  wip

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ akonadi-import-wizard akonadi-plugin-calendar \
 akonadi-plugin-contacts akonadi-plugin-contacts \
 akonadi-plugin-contacts akonadi-plugin-contacts xterm \
 mariadb mailcommon mailimporter xscreensaver partitionmanager;
-
+ 
 echo -e "\nINSTALL ZSH (please, restart.)";
 zypper install -y --auto-agree-with-licenses zsh;
 chsh -s $(which zsh);
@@ -42,7 +42,7 @@ echo -e "\nINSTALL UTILITIES";
 zypper install -y --auto-agree-with-licenses neofetch helvum ksysguard5 symbols-only-nerd-fonts \
 mariadb-client sensors xclip btop powerline-fonts ksystemlog bucklespring gimp kwrite \
 inkscape java-11-openjdk eclipse-jdt xournalpp dconf-editor protonvpn-gui simplescreenrecorder kio-gdrive;
-
+ 
 # zypper --gpg-auto-import-keys --non-interactive --quiet ar -n 'repo-ruby' \
 # -f https://download.opensuse.org/repositories/home:bmwiedemann:ruby/dlre_Tumbleweed/home:bmwiedemann:ruby.repo && \
 # zypper --gpg-auto-import-keys ref && zypper install -y --auto-agree-with-licenses ruby3.2-rubygem-lolcat && \
