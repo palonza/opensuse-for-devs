c711b842402eb9e4c7cdd2f7603bab79fdb9005c
Jоsé P. Alоnza <192836+palonza@users.noreply.github.com>
2024-06-23 23:40:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ kpat kreversi ksudoku kontact hugin kmail ktnef xscreensaver \
 akregator skanlite korganizer pim-sieve-editor \
 pim-data-exporter akonadi-calendar-tools akonadi-contact \
 akonadi-import-wizard akonadi-plugin-calendar \
-akonadi-plugin-contacts akonadi-plugin-contacts certbot \
+akonadi-plugin-contacts akonadi-plugin-contacts certbot sox \
 akonadi-plugin-contacts akonadi-plugin-contacts xterm chezmoi chezmoi-zsh-completion \
 mariadb mailcommon mailimporter xscreensaver akonadi-calendar-tools \
 akonadi-contacts akonadi-import-wizard akonadi-mime akonadi-plugin-calendar \
